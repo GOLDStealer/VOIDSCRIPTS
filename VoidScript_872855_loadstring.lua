853d6a01592d86eaf7dd3248246fb0af446ad87c
VOIDZ <goldbed145@gmail.com>
2024-12-12 11:42:56 +0800

    Upload VoidScript_872855_loadstring.lua

---
+++ VoidScript_872855_loadstring.lua
@@ -0,0 +1,2 @@
+-- This loadstring will execute the uploaded script from GitHub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VOIDv3/VOIDSCRIPTS/main/VoidScript_872855_note.lua", true))()
