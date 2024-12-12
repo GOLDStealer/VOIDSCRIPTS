b861e6e7b40fea70200d6c6a1f89c15e2d47b452
VOIDZ <goldbed145@gmail.com>
2024-12-12 10:31:18 +0800

    Add generated file: VoidScript_753777_loadstring.lua

---
+++ lua/VoidScript_753777_loadstring.lua
@@ -0,0 +1,2 @@
+-- This loadstring will execute the combined script uploaded to GitHub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VOIDv3/VOIDSCRIPTS/main/lua/VoidScript_753777_combined.lua", true))()
